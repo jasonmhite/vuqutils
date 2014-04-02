4857a31914b370cffd5d7cd4203048864f8e163f
Jason Hite <jasonmhite@gmail.com>
2014-04-01 20:47:24 -0400

    Try and make distutils work properly

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-#from distutils.core import setup
-from setuptools import setup
+from distutils.core import setup
+#from setuptools import setup
 
 setup(
     name='vuqutils',
     version='0.0.2',
     author='Jason M. Hite',
     author_email='jasonmhite@gmail.com',
-    packages=['vuqutils'],
+    packages=['vuqutils',
+        'vuqutils.files',
+        'vuqutils.math',
+        'vuqutils.plot',
+        ],
     license='LICENSE.txt',
     description='Useful stuff for VUQ warriors.',
     long_description=open('README.txt').read(),
