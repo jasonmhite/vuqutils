074c165d08cda03aded36c545098e2c2e210ce34
Jason Hite <jasonmhite@gmail.com>
2014-04-01 20:10:02 -0400

    Include script for reading sensitivites
    Including a system-wide script sdf2hdf5 to read an sdf file and dump an
    hdf5 file.

--- setup.py
+++ setup.py
@@ -3,19 +3,18 @@ from setuptools import setup
 
 setup(
     name='vuqutils',
-    version='0.0.1',
+    version='0.0.2',
     author='Jason M. Hite',
     author_email='jasonmhite@gmail.com',
     packages=['vuqutils'],
-    #url='http://pypi.python.org/pypi/TowelStuff/',
     license='LICENSE.txt',
     description='Useful stuff for VUQ warriors.',
     long_description=open('README.txt').read(),
     install_requires=[
-        "seaborn"
+        "seaborn",
+        "pytables"
     ],
-    #classifiers=[
-        #'Intended Audience :: Science/Research',
-        #'License :: OSI Approvied :: BSD License',
-    #],
+    scripts=[
+        'bin/sdf2hdf5',
+    ]
 )
